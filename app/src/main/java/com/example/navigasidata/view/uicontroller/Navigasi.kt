dfe0d4d1d33677f518817d19112458a903a0d47e
finanovita <finanovita479@gmail.com>
2025-11-21 15:50:19 +0700

    menambahkan enum navigasi untuk Formulirku dan Detail

---
+++ app/src/main/java/com/example/navigasidata/view/uicontroller/Navigasi.kt
@@ -0,0 +1,27 @@
+package com.example.navigasidata.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasidata.model.DataJK.JenisK
+import com.example.navigasidata.view.FormIsian
+import com.example.navigasidata.view.TampilData
+import com.example.navigasidata.viewmodel.SiswaViewModel
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+
